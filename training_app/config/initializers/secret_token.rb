07c00dbfdc989be54ca8b0283caea095587025e6
rupalipar <rupalipar@cybage.com>
2014-08-19 15:36:55 +0530

    Added new repository

---
+++ training_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainingApp::Application.config.secret_token = 'a7fb4cc0e70105310db8e3d08449018640fc76f3f32af33f023f0062b096ea68ff64324962b250d56b0f29ae3404d74d3d5a42388455fc8606a1e742f87fcc12'
